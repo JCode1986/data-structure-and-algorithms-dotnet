afd609d319b851e9b457d1338e43a329d6163746
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-03-21 14:19:06 -0700

    tests added for insert method

--- data-structures/LinkList/LinkListTest/UnitTest1.cs
+++ data-structures/LinkList/LinkListTest/UnitTest1.cs
@@ -1,3 +1,4 @@
+using LinkedList.Classes;
 using LinkList.Classes;
 using System;
 using Xunit;
@@ -29,6 +30,48 @@ namespace LinkListTest
             node.Value = 15;
             Assert.Equal(15, node.Value);
         }
-        
+
+        [Fact]
+        public void NodeClassHasNextProperty()
+        {
+            Node node = new Node();
+            Assert.Null(node.Next);
+        }
+
+        [Fact]
+        public void NextProperyOnNodeCanBeSet()
+        {
+            Node node = new Node();
+            node.Value = 18;
+
+            Node node2 = new Node();
+            node.Next = node2;
+
+            Assert.Equal(node.Next, node2);
+        }
+
+        [Fact]
+        public void LinkedListClassHasHeadProperty()
+        {
+            Linklist linkList = new Linklist();
+            Assert.Null(linkList.Head);
+        }
+
+        [Fact]
+        public void CanInsertNewNodeToLinkedListAsHead()
+        {
+            Linklist ll = new Linklist();
+            ll.Insert(10);
+            Assert.Equal(10, ll.Head.Value);
+        }
+
+        [Fact]
+        public void CanInsertNewNodeAsHeadInLLThatAlreadyHasNodes()
+        {
+            Linklist ll = new Linklist();
+            ll.Insert(10);
+            ll.Insert(20);
+            Assert.Equal(20, ll.Head.Value);
+        }
     }
 }
