e5150aa03e4705ffcc91b138e0bb05035c208f57
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-03-20 20:37:20 -0700

    first commit on visual studio, copied existing code from visual code

---
+++ challenges/ArrayReverse/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayReverse
+{
+    class Program
+    {
+        public static string GetNumbersInArray(int[] arr)
+        {
+            string str = "Array: { ";
+            foreach (int num in arr)
+            {
+                str += $"{Convert.ToString(num)}, ";
+            }
+            return $"{str}}}";
+        }
+
+        public static void GetUserInputArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Index {i}: Enter element: ");
+                array[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine(GetNumbersInArray(array));
+            }
+        }
+
+        public static string ArrayReverse()
+        {
+            Console.WriteLine("Enter array size: ");
+            int arraySize = int.Parse(Console.ReadLine());
+            int[] arrayInput = new int[arraySize];
+            GetUserInputArray(arrayInput);
+            int[] result = new int[arraySize];
+            int j = 0;
+            for (int i = arrayInput.Length - 1; i >= 0; i--)
+            {
+                result[j] = arrayInput[i];
+                j++;
+            }
+            return $"\nInput-->{GetNumbersInArray(arrayInput)}\nOutput-->{GetNumbersInArray(result)}";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ArrayReverse());
+        }
+    }
+}
---
+++ challenges/ArrayReverse/README.md
@@ -0,0 +1,14 @@
+﻿# Reverse an Array
+<!-- Short summary or background information -->
+
+## Challenge
+<!-- Description of the challenge -->
+* Write a function called reverseArray which takes an array as an argument. Without utilizing any of the built-in methods available to your language, return an array with elements in reversed order.
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+* Initialized a `result` array, and utilized a for loop starting from the end of input array. Insert elements from `index -1` from input array to `index 0` of `result` array until loop is complete.
+
+* Big O  
+    * Time and Space - O(n); iterating through every element in array, and inserting element in `result` array.
+
