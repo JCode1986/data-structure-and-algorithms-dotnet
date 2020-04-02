d7820197f1f0f421e0a74cc2d9fbe78f848324a2
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-04-01 22:00:26 -0700

    initial commit

---
+++ challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MultiBracketValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// Method that returns a boolean if all brackets has an opening and closing match
+        /// </summary>
+        /// <param name="brackets">string</param>
+        /// <returns>boolean</returns>
+        public bool BracketValidation(string brackets)
+        {
+            return true;
+        }
+    }
+}
