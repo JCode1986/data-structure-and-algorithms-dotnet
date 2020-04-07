363882f85375acb3205ab0c07515be1ecb07085e
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-04-07 10:57:32 -0700

    attempted fizzbuzz method. still needs work.

--- challenges/FizzBuzzTree/FizzBuzzTree/Classes/FizzBuzzTree.cs
+++ challenges/FizzBuzzTree/FizzBuzzTree/Classes/FizzBuzzTree.cs
@@ -1,11 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Tree;
+using Tree.Classes;
+using Trees.Classes;
 
 namespace FizzBuzzTree.Classes
 {
-    public class FizzBuzzTree
+    public class MyFizzBuzzTree
     {
+        
+        public void FBTree(Node<int> node)
+        {
+            BinaryTree<string> BT = new BinaryTree<string>();
 
+            if (node == null)
+            {
+                return;
+            }
+
+            if (node.Left != null)
+            {
+                FBResults(node.Left);
+            }
+
+            if (node.Right != null)
+            {
+                FBResults(node.Right);
+            }
+        }
+
+        /// <summary>
+        /// Helper function to calculate fizzbuzz
+        /// </summary>
+        /// <param name="value">int</param>
+        /// <returns>string</returns>
+        public static string FBResults(Node<int> node)
+        {
+            string str = "";
+            if (node.Value % 3 == 0)
+            {
+                str += "Fizz";
+            }
+            if (node.Value % 3 == 0)
+            {
+                str += "Buzz";
+            }
+            else
+            {
+                str += Convert.ToString(node.Value);
+            }
+            return str;
+        }
     }
 }
