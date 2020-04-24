6da95f3d0f7932957745db514f36fc994a6da8cd
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-04-24 16:06:06 -0700

    added contructor for hashtable with test; hash method completed with test

--- data-structures/HashTable/HashTable/Classes/HashTable.cs
+++ data-structures/HashTable/HashTable/Classes/HashTable.cs
@@ -4,7 +4,45 @@ using System.Text;
 
 namespace HashTable.Classes
 {
-    class HashTable
+    public class MyHashTable<T>
     {
+        public int Size { get; set; }
+        public T[] Table { get; set; }
+
+        /// <summary>
+        /// Instantiate hash table with with size of array
+        /// </summary>
+        /// <param name="size"></param>
+        public MyHashTable(int size)
+        {
+            Size = size;
+            Table = new T[size];
+        }
+
+        /// <summary>
+        /// Hashes every character in string and returns an integer which will be the index
+        /// </summary>
+        /// <param name="key">string</param>
+        /// <returns>int</returns>
+        public int Hash(T key)
+        {
+            int hashedKey = 0;
+
+            foreach (char character in key.ToString())
+            {
+                hashedKey += Convert.ToInt32(character);
+            }
+            return (hashedKey * 599) % Table.Length;
+        }
+
+        /// <summary>
+        /// Adds a key value in the hashtable
+        /// </summary>
+        /// <param name="key">int</param>
+        /// <param name="value">string</param>
+        public void Add(T key, T value)
+        {
+
+        }
     }
 }
--- data-structures/HashTable/XUnitTestHashTable/UnitTest1.cs
+++ data-structures/HashTable/XUnitTestHashTable/UnitTest1.cs
@@ -1,3 +1,4 @@
+using HashTable.Classes;
 using System;
 using Xunit;
 
@@ -6,9 +7,23 @@ namespace XUnitTestHashTable
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanInstantiateHashTableWithSize()
         {
+            MyHashTable<object> table = new MyHashTable<object>(50);
+            Assert.Equal(50, table.Table.Length);
+        }
 
+        [Theory]
+        [InlineData("Hello", 36)]
+        [InlineData("Foo", 2)]
+        [InlineData("Bar", 37)]
+        [InlineData("1234", 7)]
+        [InlineData("FooBar", 39)]
+        public void CanHashAKey(string key, int expected)
+        {
+            MyHashTable<object> table = new MyHashTable<object>(41);
+            int actual = table.Hash(key);
+            Assert.Equal(expected, actual);
         }
     }
 }
