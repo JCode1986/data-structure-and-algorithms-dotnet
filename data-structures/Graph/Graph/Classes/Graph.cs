bc5f273f7d3419f403e4daf64e6f48eae1be0ed3
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-05-10 16:39:41 -0700

    added tests

--- data-structures/Graph/Graph/Classes/Graph.cs
+++ data-structures/Graph/Graph/Classes/Graph.cs
@@ -4,18 +4,22 @@ using System.Text;
 
 namespace Graph.Classes
 {
-    class Graph<T>
+    public class MyGraph<T>
     {
         public Dictionary<Vertex<T>, List<Edge<T>>> AdjacencyList { get; set; }
 
         private int _size;
 
-        public Graph()
+        public MyGraph()
         {
             AdjacencyList = new Dictionary<Vertex<T>, List<Edge<T>>>();
         }
 
-        // Add Node/Vertex to the graph/adjacency List
+        /// <summary>
+        /// Adds a vertex to adjacency list, and increments list size by 1
+        /// </summary>
+        /// <param name="value">T</param>
+        /// <returns>node</returns>
         public Vertex<T> AddNode(T value)
         {
             Vertex<T> node = new Vertex<T>(value);
@@ -24,7 +28,12 @@ namespace Graph.Classes
             return node;
         }
 
-        // add edge
+        /// <summary>
+        /// Add edge one way
+        /// </summary>
+        /// <param name="a">vertex</param>
+        /// <param name="b">vertex</param>
+        /// <param name="weight">int</param>
         public void AddDirectedEdge(Vertex<T> a, Vertex<T> b, int weight)
         {
             AdjacencyList[a].Add(
@@ -32,17 +41,26 @@ namespace Graph.Classes
                 {
                     Vertex = b,
                     Weight = weight
-
                 }
-                );
+            );
         }
 
+        /// <summary>
+        /// Add edge two-way
+        /// </summary>
+        /// <param name="a">vertex</param>
+        /// <param name="b">vertex</param>
+        /// <param name="weight">int</param>
         public void AddUndirectedEdge(Vertex<T> a, Vertex<T> b, int weight)
         {
             AddDirectedEdge(a, b, weight);
             AddDirectedEdge(b, a, weight);
         }
 
+        /// <summary>
+        /// Returns a list of vertices
+        /// </summary>
+        /// <returns>list</returns>
         public List<Vertex<T>> GettAllVertices()
         {
             List<Vertex<T>> vertices = new List<Vertex<T>>();
@@ -55,30 +73,42 @@ namespace Graph.Classes
             return vertices;
         }
 
-        // Get Neighbors
+        /// <summary>
+        /// Get all neighbors from a specific vertex
+        /// </summary>
+        /// <param name="vertex">vetex</param>
+        /// <returns>List of </returns>
         public List<Edge<T>> GetNeighbors(Vertex<T> vertex)
         {
             // get all edges connected to a vertex
             return AdjacencyList[vertex];
         }
 
+        /// <summary>
+        /// returns size of adjacency list
+        /// </summary>
+        /// <returns>int</returns>
         public int Size()
         {
             return _size;
         }
 
-        public void Print()
+        /// <summary>
+        /// Prints out all vertices and edges in adjacency list
+        /// </summary>
+        public string Print()
         {
+            string result = "Vertices: ";
             foreach (var vertex in AdjacencyList)
             {
-                Console.Write($"{vertex.Key.Value}: ");
-
                 foreach (var edge in vertex.Value)
                 {
-                    Console.Write($"{edge.Vertex.Value}, {edge.Weight} ->");
+                    //Console.Write($"{edge.Vertex.Value}, {edge.Weight} ->");
+                    result += $"{vertex.Key.Value} and {edge.Vertex.Value}, Weight: {edge.Weight} --> ";
                 }
-                Console.WriteLine("null");
             }
+            result += "null";
+            return result;
         }
     }
 }
