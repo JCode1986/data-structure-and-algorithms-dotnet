36bc1c05313e492b749e81eb2dfafc71cb2f7cef
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-08-13 23:48:24 -0700

    selection sort completed with tests

--- sorts/selection-sort/XUnitTestSelectionSort/UnitTest1.cs
+++ sorts/selection-sort/XUnitTestSelectionSort/UnitTest1.cs
@@ -1,3 +1,4 @@
+using SelectionSort;
 using System;
 using Xunit;
 
@@ -5,10 +6,16 @@ namespace XUnitTestSelectionSort
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData(new int[] { 3, 2, 1 }, new int[] { 1, 2, 3 })]
+        [InlineData(new int[] { 3, 2, 1, 6, 9, 8, 5, 4, 7 }, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 })]
+        [InlineData(new int[] { 3, 2 }, new int[] { 2, 3 })]
+        [InlineData(new int[] { 1 }, new int[] { 1 })]
 
+        public void Test1(int[] array, int[] expected)
+        {
+            int[] actual = MySelectionSort.SSort(array);
+            Assert.Equal(expected, actual);
         }
     }
 }
